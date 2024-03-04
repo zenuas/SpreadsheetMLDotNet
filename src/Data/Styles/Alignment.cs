6e55f34244184b3da9125c7bcfa59f9551b6aa39
zenuas@gmail.com <zenuas@gmail.com>
2024-03-04 22:35:25 +0900

    アライメントにIEquatableを実装

--- src/Data/Styles/Alignment.cs
+++ src/Data/Styles/Alignment.cs
@@ -1,6 +1,8 @@
-﻿namespace SpreadsheetMLDotNet.Data.Styles;
+﻿using System;
 
-public class Alignment
+namespace SpreadsheetMLDotNet.Data.Styles;
+
+public class Alignment : IEquatable<Alignment>
 {
     public HorizontalAlignmentTypes? HorizontalAlignment { get; set; }
     public uint? Indent { get; set; }
@@ -11,4 +13,37 @@ public class Alignment
     public uint? TextRotation { get; set; }
     public VerticalAlignmentTypes? VerticalAlignment { get; set; }
     public bool? WrapText { get; set; }
+
+    public bool Equals(Alignment? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (!Equals(HorizontalAlignment, other.HorizontalAlignment)) return false;
+        if (!Equals(Indent, other.Indent)) return false;
+        if (!Equals(JustifyLastLine, other.JustifyLastLine)) return false;
+        if (!Equals(ReadingOrder, other.ReadingOrder)) return false;
+        if (!Equals(RelativeIndent, other.RelativeIndent)) return false;
+        if (!Equals(ShrinkToFit, other.ShrinkToFit)) return false;
+        if (!Equals(TextRotation, other.TextRotation)) return false;
+        if (!Equals(VerticalAlignment, other.VerticalAlignment)) return false;
+        if (!Equals(WrapText, other.WrapText)) return false;
+        return true;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Alignment);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(HorizontalAlignment);
+        hash.Add(Indent);
+        hash.Add(JustifyLastLine);
+        hash.Add(ReadingOrder);
+        hash.Add(RelativeIndent);
+        hash.Add(ShrinkToFit);
+        hash.Add(TextRotation);
+        hash.Add(VerticalAlignment);
+        hash.Add(WrapText);
+        return hash.ToHashCode();
+    }
 }
