62c5d86f819ce4d95c362db0cb09fa0d2c10576b
zenuas@gmail.com <zenuas@gmail.com>
2024-03-06 00:57:46 +0900

    WriteLineの定義を見直し

--- src/Extension/Streams.cs
+++ src/Extension/Streams.cs
@@ -5,5 +5,11 @@ namespace SpreadsheetMLDotNet.Extension;
 
 public static class Streams
 {
-    public static void WriteLine(this Stream self, string s) => self.Write(Encoding.UTF8.GetBytes($"{s}\r\n"));
+    public static void WriteLine(this Stream self) => self.Write(Encoding.UTF8.GetBytes("\r\n"));
+
+    public static void WriteLine(this Stream self, string s)
+    {
+        self.Write(Encoding.UTF8.GetBytes(s));
+        self.WriteLine();
+    }
 }
