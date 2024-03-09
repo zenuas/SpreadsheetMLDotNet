c26c6a79857cfdf544cdca4e2fc2897851165bd8
zenuas@gmail.com <zenuas@gmail.com>
2024-03-09 13:11:45 +0900

    BorderPropertiesTypeのstruct変更漏れを解消

--- src/Data/Styles/BorderPropertiesType.cs
+++ src/Data/Styles/BorderPropertiesType.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace SpreadsheetMLDotNet.Data.Styles;
 
-public class BorderPropertiesType : IEquatable<BorderPropertiesType>
+public struct BorderPropertiesType()
 {
     public Color? Color { get; set; }
     public BorderStyles Style { get; set; } = BorderStyles.None;
-
-    public bool Equals(BorderPropertiesType? other)
-    {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-        if (!Equals(Color, other.Color)) return false;
-        if (!Equals(Style, other.Style)) return false;
-        return true;
-    }
-
-    public override bool Equals(object? obj) => Equals(obj as BorderPropertiesType);
-
-    public override int GetHashCode() => HashCode.Combine(Color, Style);
 }
