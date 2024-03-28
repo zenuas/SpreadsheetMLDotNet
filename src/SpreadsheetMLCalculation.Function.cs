44a33100f1810c17c99e4a8e52fff7234d8466c6
zenuas@gmail.com <zenuas@gmail.com>
2024-03-29 00:01:11 +0900

    リファクタリング

--- src/SpreadsheetMLCalculation.Function.cs
+++ src/SpreadsheetMLCalculation.Function.cs
@@ -23,7 +23,7 @@ public static partial class SpreadsheetMLCalculation
         if (values[0] is Calculation.Range range)
         {
             r = new CellValueDouble { Value = 0 };
-            foreach (var e in EnumerableCells(GetWorksheet(calc, current_sheet, range), range.From, range.To))
+            foreach (var e in EnumerableAddressToCells(GetWorksheet(calc, current_sheet, range), range.From, range.To))
             {
                 var x = EvaluateDouble(r, "+", e.Cell);
                 if (x is not CellValueDouble r2) return CellValueError.VALUE;
@@ -42,60 +42,63 @@ public static partial class SpreadsheetMLCalculation
 
     public static Worksheet GetWorksheet(Dictionary<string, WorksheetCalculation> calc, Worksheet current_sheet, Calculation.Range range) => range.SheetName == "" ? current_sheet : calc[range.SheetName].Worksheet;
 
-    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableCells(Worksheet sheet, IAddress from, IAddress to)
+    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableAddressToCells(Worksheet sheet, IAddress from, IAddress to)
     {
         if (from is RowAddress r)
         {
             var start = r.Row;
             var end = to.Cast<RowAddress>().Row;
-            return EnumerableRowsToCell(sheet, Math.Min(start, end), Math.Max(start, end));
+            return EnumerableRowsToCells(sheet, Math.Min(start, end), Math.Max(start, end));
         }
         if (from is ColumnAddress c)
         {
             var start = c.Column;
             var end = to.Cast<ColumnAddress>().Column;
-            return EnumerableColumnsToCell(sheet, Math.Min(start, end), Math.Max(start, end));
+            return EnumerableColumnsToCells(sheet, Math.Min(start, end), Math.Max(start, end));
         }
         else
         {
             var start = from.Cast<CellAddress>();
             var end = to.Cast<CellAddress>();
-            return EnumerableRangeToCell(sheet, Math.Min(start.Row, end.Row), Math.Max(start.Row, end.Row), Math.Min(start.Column, end.Column), Math.Max(start.Column, end.Column));
+            return EnumerableRangeToCells(sheet, Math.Min(start.Row, end.Row), Math.Max(start.Row, end.Row), Math.Min(start.Column, end.Column), Math.Max(start.Column, end.Column));
         }
     }
 
-    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableRowsToCell(Worksheet sheet, int from, int to)
+    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableRowsToCells(Worksheet sheet, int from, int to)
     {
         foreach (var (row, row_value) in sheet.GetRowsExists(from, to))
         {
-            for (var column = 0; column < row_value.Cells.Count; column++)
+            for (var x = 0; x < row_value.Cells.Count; x++)
             {
-                yield return (row, column + row_value.Cells.StartIndex, row_value.Cells[column].Value);
+                yield return (row, x + row_value.Cells.StartIndex, row_value.Cells[x].Value);
             }
         }
     }
 
-    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableColumnsToCell(Worksheet sheet, int from, int to)
+    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableColumnsToCells(Worksheet sheet, int from, int to)
     {
-        for (var row = 0; row < sheet.Rows.Count; row++)
+        for (var y = 0; y < sheet.Rows.Count; y++)
         {
-            var row_value = sheet.Rows[row];
-            for (var column = Math.Max(0, from - row_value.Cells.StartIndex); column < row_value.Cells.Count; column++)
+            var row = y + sheet.Rows.StartIndex;
+            var row_value = sheet.Rows[y];
+            for (var x = Math.Max(0, from - row_value.Cells.StartIndex); x < row_value.Cells.Count; x++)
             {
-                if (column + row_value.Cells.StartIndex > to) break;
-                yield return (row + sheet.Rows.StartIndex, column + row_value.Cells.StartIndex, row_value.Cells[column].Value);
+                var col = x + row_value.Cells.StartIndex;
+                if (col > to) break;
+                yield return (row, col, row_value.Cells[x].Value);
             }
         }
     }
 
-    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableRangeToCell(Worksheet sheet, int row_from, int row_to, int col_from, int col_to)
+    public static IEnumerable<(int Row, int Column, ICellValue Cell)> EnumerableRangeToCells(Worksheet sheet, int row_from, int row_to, int col_from, int col_to)
     {
         foreach (var (row, row_value) in sheet.GetRowsExists(row_from, row_to))
         {
-            for (var column = Math.Max(0, col_from - row_value.Cells.StartIndex); column < row_value.Cells.Count; column++)
+            for (var x = Math.Max(0, col_from - row_value.Cells.StartIndex); x < row_value.Cells.Count; x++)
             {
-                if (column + row_value.Cells.StartIndex > col_to) break;
-                yield return (row, column + row_value.Cells.StartIndex, row_value.Cells[column].Value);
+                var col = x + row_value.Cells.StartIndex;
+                if (col > col_to) break;
+                yield return (row, col, row_value.Cells[x].Value);
             }
         }
     }
